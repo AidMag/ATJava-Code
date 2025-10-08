2b5d728819a19b93714f8beb4fc2144eed30949a
AidMag <27amagnus@derryfield.org>
2025-10-08 14:07:16 -0400

    Update PartC.java
    Finish Problem with explanation of why the original code would not work and the modified code.

--- Lesson06/PartC.java
+++ Lesson06/PartC.java
@@ -2,18 +2,19 @@
 Explain why the following program does not produce the output that some people would expect it to produce:
     
     The following output would not produce what people would expect becuase the way floating-point values are stored.
-They are storage methoud introduces error, which is not noticable by humans, which makes it so comparing the values  
-
-Fix the program so it will produce the output that most people would expect it to produce.  Submit your corrected program for grading through MSA or GitHub.
-
+They are stored in a limited space so when proforming calcualtions it introduces rounding errors. These slight differences 
+causes the values to be slighly different, causing the program to say they are not equal.
  */
 
 public class PartC {
     public static void main(String[] args) {
+        
         double a = 1.2; 
         double b= 3.0;
         double c = a * b; 
-        if(c == 3.6){
+        final double Threshold = 1e-14;
+
+        if(Math.abs(c-3.6) < Threshold){
         System.out.println("c is 3.6");
         } else {
         System.out.println("c is not 3.6");
