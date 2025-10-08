6f36d176bf40c143c8506ddb86767e7e570b54ac
AidMag <27amagnus@derryfield.org>
2025-10-08 12:19:58 -0400

    Update PartB.java
    Took out the HW question and inserted my name.

--- Lesson06/PartB.java
+++ Lesson06/PartB.java
@@ -1,16 +1,5 @@
 /*
- Fix the following section of code so that it will run properly.
- Submit your code as a Java source file for grading using MSA or GitHub.
-// prompt user for age.
-Scanner input = new Scanner(System.in);
-System.out.print("Enter your age: ");
-int age = input.nextInt();
-
-// are they lucky?
-if (age = 13) {
-System.out.println("Lucky 13!");
-}
-
+Aidan
  */
 
 import java.util.Scanner;
