78502625b7da2ae978a5c4d7a9ecb7cb3c0aded4
AidMag <27amagnus@derryfield.org>
2025-10-08 12:18:56 -0400

    Update PartB.java
    Completed the problem by fixing the errors in the code

--- Lesson06/PartB.java
+++ Lesson06/PartB.java
@@ -1,5 +1,32 @@
+/*
+ Fix the following section of code so that it will run properly.
+ Submit your code as a Java source file for grading using MSA or GitHub.
+// prompt user for age.
+Scanner input = new Scanner(System.in);
+System.out.print("Enter your age: ");
+int age = input.nextInt();
+
+// are they lucky?
+if (age = 13) {
+System.out.println("Lucky 13!");
+}
+
+ */
+
+import java.util.Scanner;
+
 public class PartB {
     public static void main(String[] args) {
+        // prompt user for age.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        // are they lucky?
+        if (age == 13) {
+        System.out.println("Lucky 13!");
+        }
         
+        input.close();
     }
 }
