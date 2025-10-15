0520f3c33eef661f8814a71b6faec72d368f5492
AidMag <27amagnus@derryfield.org>
2025-10-15 17:43:27 -0400

    Update PartB.java
    Comments added

--- Lesson06-Programing-Projects/PartB.java
+++ Lesson06-Programing-Projects/PartB.java
@@ -9,12 +9,15 @@ public class PartB {
         
         Scanner in = new Scanner(System.in);
 
+        //Getting variable values
         System.out.print("Enter two floating-point numbers: ");
         double num_1 = in.nextDouble();
         double num_2 = in.nextDouble();
 
+        //declaring constant THRESHOLD
         final double THRESHOLD = 1e-2;
 
+        //Conditional statement that determines if the numbers are equal
         if (Math.abs(num_1 - num_2) < THRESHOLD) {
             System.out.print("They are the same up to two decimal places.");
         } else {
