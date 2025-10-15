1114d899a865756448634f595e3cf452540534fa
AidMag <27amagnus@derryfield.org>
2025-10-15 17:39:29 -0400

    Update PartA.java
    Taken out problem and inserted name

--- Lesson06-Programing-Projects/PartA.java
+++ Lesson06-Programing-Projects/PartA.java
@@ -1,10 +1,5 @@
 /*
-Write a program that reads a temperature value and the letter C for Celsius or F for Fahrenheit.
-Print whether water is liquid, solid, or gaseous at the given temperature at sea level.
-Use a freezing point of 0 deg C and a boiling point of 100 deg C for this example.
-Your program run should look something like the following,
-Enter a temperature and a 'C' for Celsius or 'F' for Fahrenheit: 212 F
-At a temperature of 212.00 F, water is in a gaseous state (i.e., steam).
+Aidan
  */
 
 import java.util.Scanner;
