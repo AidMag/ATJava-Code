563d9caae80715bdb53fcca2cb94e9fbfe7f369a
AidMag <27amagnus@derryfield.org>
2025-10-18 07:19:11 -0400

    Update PartC.java
    Adding descriptions for each section of code.

--- Lesson06-Programing-Projects/PartC.java
+++ Lesson06-Programing-Projects/PartC.java
@@ -1,9 +1,5 @@
 /*
-Write a program that reads in three strings and sorts them lexicographically.
-Enter three strings: Charlie Able Baker
-Able
-Baker
-Charlie
+Aidan
  */
 
 
@@ -12,13 +8,14 @@ import java.util.Scanner;
 public class PartC {
     public static void main(String[] args) {
         
+        //Using Scanner to get the words/names.
         Scanner in = new Scanner(System.in);
-
         System.out.print("Enter three words: ");
         String name1 = in.next();
         String name2 = in.next();
         String name3 = in.next();
 
+        //If-else cascade to alphabetize the names.
         if (name1.compareTo(name2) <= 0 && name2.compareTo(name3) <= 0) {
             System.out.println(name1);
             System.out.println(name2);
