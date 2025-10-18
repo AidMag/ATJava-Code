34cc7e1d39705fbc9816060703a97e83ac499303
AidMag <27amagnus@derryfield.org>
2025-10-18 07:16:14 -0400

    Update PartC.java
    Add the If-else cascade

--- Lesson06-Programing-Projects/PartC.java
+++ Lesson06-Programing-Projects/PartC.java
@@ -15,10 +15,35 @@ public class PartC {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter three words: ");
-        String name1 = in.nextLine();
-        String name2 = in.nextLine();
-        String name3 = in.nextLine();
+        String name1 = in.next();
+        String name2 = in.next();
+        String name3 = in.next();
 
-        
+        if (name1.compareTo(name2) <= 0 && name2.compareTo(name3) <= 0) {
+            System.out.println(name1);
+            System.out.println(name2);
+            System.out.println(name3);
+        } else if (name1.compareTo(name3) <= 0 && name3.compareTo(name2) <= 0) {
+            System.out.println(name1);
+            System.out.println(name3);
+            System.out.println(name2);
+        } else if (name2.compareTo(name3) <= 0 && name3.compareTo(name1) <= 0) {
+            System.out.println(name2);
+            System.out.println(name3);
+            System.out.println(name1);
+        } else if (name2.compareTo(name1) <= 0 && name1.compareTo(name3) <= 0) {
+            System.out.println(name2);
+            System.out.println(name1);
+            System.out.println(name3);
+        } else if (name3.compareTo(name1) <= 0 && name1.compareTo(name2) <= 0) {
+            System.out.println(name3);
+            System.out.println(name1);
+            System.out.println(name2);
+        } else if (name3.compareTo(name2) <= 0 && name2.compareTo(name1) <= 0) {
+            System.out.println(name3);
+            System.out.println(name2);
+            System.out.println(name1);
+        }
+        in.close();
     }
 }
