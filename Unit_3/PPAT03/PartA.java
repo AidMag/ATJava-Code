146b62e4a771c3ab56106f2942680af0035735bc
AidMag <27amagnus@derryfield.org>
2025-10-28 17:24:54 -0400

    Update PartA.java

--- Unit_3/PPAT03/PartA.java
+++ Unit_3/PPAT03/PartA.java
@@ -1,11 +1,32 @@
 import java.util.Scanner;
 
 /*
-Day of week as text (e.g., monday, fri, case-insensitive)
-Time entered in HH:MM format (using 24-hour notation)
-Age is an integer ≥ 0  Note:
-If the age of the passenger is equal to 100, set the cost to $666 and do not apply any discounts 
-Use a boolean value (true or false) to track the passenger's student status.
+Weekday (Mon-Fri): 
+$100.00 during rush hour (06:00 to 07:59, 16:00 to 17:59)
+$  90.00 otherwise
+Weekend (Sat, Sun):
+$  80.00
+
+Age Discounts:
+0 to 2 years (baby): free (final price is $0, ignore other discounts/fees)
+3 to 12 years (child): 20% discount
+13 to 59 years (adult): no age-based discount
+60+ (senior): 25% discount 
+
+Additional Discounts:
+Student: If a passenger has a current, valid student ID, then the passenger receives an additional 5% discount.
+The 5% student discount is applied to the ticket cost after the age discount was applied.
+
+Processing 24 Hour Time
+The user will enter the time in 24-hour format. For example, 6:15PM would be entered as "18:15". Your program should use String methods to parse the hour and minute from the text.
+First check to make sure the user entered in a colon (':') character in the string. If they did not, display an error message and exit the program using a return statement.
+You can use the Integer.parseInt() method to extract the hour and the minute from the text:
+Assume the hour is to the left of the ':' character.
+Assume the minute is to the right of the ':' character.
+
+Test Plan
+Before coding their solution, students should create a set of test cases that they will use to verify that their program works.
+Be sure to submit for grading your test plan and include your test results indicating whether or not each test case was successful.  
 
 
 name: Thomas Eng
@@ -29,22 +50,72 @@ Final Cost:         $    95.00
  */
 
 public class PartA {
+    enum DAYOFWEEK {MON, TUES, WED, THUR, FRI, SAT, SUN}
+    enum AGEGROUP {BABY, CHILD, ADULT, SENIOR}
     public static void main(String[] args) {
         
         Scanner in = new Scanner(System.in);
 
+        String name;
+        String day;
+        String student;
+        String time;
+        final String a = "Y";
+        final String b = "N";
+        double cost;
+        int age;
+
+
         System.out.print("Name: ");
-        String name = in.nextLine();
+        if (!in.hasNextLine()) {
+            System.err.print("ERROR: Enter your name %nProgram Terminating");
+            return;
+        } else {
+            name = in.nextLine();
+        }
+
+        System.out.print("Age: ");
+        if (!in.hasNextInt()) {
+            System.err.printf("ERROR: Enter your age %nProgram Terminating");
+        } else {
+            age = in.nextInt();
+        }
+
         System.out.print("Day: ");
-        String day = in.nextLine().toUpperCase();
-        System.out.print("Time (24h): ");//Figure out a solution to the colon
+        if (!in.hasNextLine()) {
+            System.err.print("ERROR: Enter the day %nProgram Terminating");
+            return;
+        } else {
+            day = in.nextLine().toUpperCase();
+        }
+
+        System.out.print("Time (24h): ");
+        if (!in.hasNextLine()) {
+            System.err.print("ERROR: Enter the time %nProgram Terminating");
+            return;
+        } else {
+            time = in.nextLine();
+        }
+        
         System.out.print("Student (y/n): ");
-        String student = in.nextLine().toLowerCase();
+        if (!in.hasNextLine()) {
+            System.err.printf("ERROR: Enter y or n %nProgram Terminating");
+            return;
+        } else {
+            student = in.nextLine().toUpperCase();
+        }
+
 
         System.out.printf("%n%n--------------------------------------------------------%n");
         System.out.printf("Name:   %s", name);
-        System.out.printf("Day: ");
-
-
+        System.out.printf("Day:      ", day);
+        System.out.printf("Time:     ", time);
+        System.out.printf("%n%n--------------------------------------------------------%n");
+        
+        
+        System.out.printf("%n%n--------------------------------------------------------%n");
+        
+        
+        System.out.printf("%n%n--------------------------------------------------------%n");
     }
 }
