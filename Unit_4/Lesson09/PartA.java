1be69595e6c276fea5d6f439c086ad97e4076f5c
AidMag <27amagnus@derryfield.org>
2025-11-05 07:32:28 -0500

    Update PartA.java
    Finished tested code

--- Unit_4/Lesson09/PartA.java
+++ Unit_4/Lesson09/PartA.java
@@ -4,6 +4,9 @@ Print instructions for the user on how to enter data before looping.
 Use a loop to get test scores from user
 Assume user enters double values only (no need to do input validation)
 User enters a negative number to quit entering data
+After the data has been entered, print results:
+Print the number of scores that were entered (not including the negative number)
+Print the average test score
  */
 
 import java.util.Scanner;
@@ -11,7 +14,7 @@ import java.util.Scanner;
 public class PartA {
    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter each score as a double. When you have entered all your scores type a negitive number. ");
+        System.out.print("Enter each score as a double. When you have entered all your scores enter a negitive number. ");
         
         int n = 0;
         double average = 0.0;
@@ -20,18 +23,19 @@ public class PartA {
 
 
         while (score > 0){
-            n = n + 1;
-            System.out.printf("%nEnter score %d: ", n);
-            score = in.nextDouble();
             if (score > 0){
-                sum = score;
-            } else {
-                break;
+                n = n + 1;
+                System.out.printf("%nEnter score %d: ", n);
+                score = in.nextDouble();
+                sum += score;
             }
         }
+        
+        sum = sum - score;
+        n = n - 1;
+        average = sum /n;
 
         if (n > 0) {
-            average = sum / n;
             System.out.printf("You averaged %.2f in %d tests!", average, n);
         } else {
             System.err.println("No valid scores were entered.");
