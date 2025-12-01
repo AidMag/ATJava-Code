ceb63dd20ff303ea9772f9acfe0a449a4380e1c2
AidMag <27amagnus@derryfield.org>
2025-12-01 13:58:26 -0500

    Prime num
    Almost done, one logic error: I think.

--- Unit_4/ProgramingChallenges/Prime_num.java
+++ Unit_4/ProgramingChallenges/Prime_num.java
@@ -6,8 +6,30 @@ import java.util.Scanner;
 
 public class Prime_num {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
         
+        int num, i;
+    
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a integer: ");
+        num = in.nextInt();
+
+
+        for (i = 1; i <= num; i++) {
+            double l = num % i;
+            if (l == 0) {
+                System.out.print("This number is not prime");
+                break;
+            } else {
+                continue;
+            }
+        }
+        
+
+        if (i == num && num != 1) {
+            System.out.print("This number is prime");
+        }
+        
+        in.close();
     }
 }
