db796523445194ac693d9990a17650348941534c
AidMag <27amagnus@derryfield.org>
2025-12-01 12:41:37 -0500

    Update Dicerolling.java
    copy and pasting the prompt and the code I made for an earlier project to create the die.

--- Unit_4/ProgramingChallenges/Dicerolling.java
+++ Unit_4/ProgramingChallenges/Dicerolling.java
@@ -1,5 +1,58 @@
+/*
+Write a program that simulates the rolling of two six sided dice:
+Display the output of each dice roll to the user
+Stop when the two dice have the same number
+Bonus, can you display the output as ASCII art dice?
+
++-------+      +-------+
+| O   O |      | O   O |
+|   O   |      |   O   |
+| O   O |      | O   O |
++-------+      +-------+
+
+ */
+
+import java.util.Scanner;
+
 public class Dicerolling {
     public static void main(String[] args) {
+        import java.util.Scanner;
+
+public class PartD {
+    public static void main(String[] args) {
         
+        Scanner in = new Scanner(System.in);
+
+        String input;
+        int num = 0;
+
+        while (true){
+            System.out.print("Enter 'r' to roll, 'd' to display, or 'q' to quit: ");
+            input = in.nextLine().toUpperCase();
+
+            if (input.equals("R")){
+                num = (int)(Math.random() * 7);
+            } else if (input.equals("D")){
+                if (num == 1){
+                    System.out.printf("+-------+ %n|       | %n|   0   | %n|       | %n+-------+ %n");
+                } else if (num == 2){
+                    System.out.printf("+-------+ %n| 0     | %n|       | %n|     0 | %n+-------+ %n");
+                } else if (num == 3){
+                    System.out.printf("+-------+ %n| 0     | %n|   0   | %n|     0 | %n+-------+ %n");
+                } else if (num == 4){
+                    System.out.printf("+-------+ %n| 0   0 | %n|       | %n| 0   0 | %n+-------+ %n");
+                } else if (num == 5){
+                    System.out.printf("+-------+ %n| 0   0 | %n|   0   | %n| 0   0 | %n+-------+ %n");
+                } else if (num == 6){
+                    System.out.printf("+-------+ %n| 0   0 | %n| 0   0 | %n| 0   0 | %n+-------+ %n");
+                }
+            } else if (input.equals("Q")){
+                break;
+            }
+        }
+
+        in.close();
+    }
+}
     }
 }
