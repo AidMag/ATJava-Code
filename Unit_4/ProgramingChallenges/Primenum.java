cb132d4d80483e55cffbef96bd2518822360b970
AidMag <27amagnus@derryfield.org>
2025-12-01 13:43:36 -0500

    Update Primenum.java

--- Unit_4/ProgramingChallenges/Primenum.java
+++ Unit_4/ProgramingChallenges/Primenum.java
@@ -1,7 +1,7 @@
 /*
 Write a program that asks the user for an integer and checks to determine whether or not the function is prime.
 */
-
+THIS IS NOT THE FINAL: SEE THE OTHER JAVA FILE CALLED "Prime_num"
 import java.util.Scanner;
 
 public class Primenum {
