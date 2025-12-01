2fd6c1955b93fc15f47818c2ecf2c105bb9edaf9
AidMag <27amagnus@derryfield.org>
2025-12-01 12:40:10 -0500

    Update Panlindrome.java

--- Unit_4/ProgramingChallenges/Panlindrome.java
+++ Unit_4/ProgramingChallenges/Panlindrome.java
@@ -1,14 +1,5 @@
 /*
-A palindrome is a word or phrase that reads the same forwards and backwards. 
-The following words are examples of palindromes,
-
-madam
-kayak
-mom
-dad
-
-Write a program that prompts the user for a word or phrase and then uses a loop to check whether or not the word or phrase is a palindrome.
-
+Aidan
 */
 
 import java.util.Scanner;
