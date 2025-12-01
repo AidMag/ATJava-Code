3f072db0806ed9c1e637277fb228fd9d5d722986
AidMag <27amagnus@derryfield.org>
2025-12-01 13:42:08 -0500

    Create Prime_num.java

---
+++ Unit_4/ProgramingChallenges/Prime_num.java
@@ -0,0 +1,13 @@
+/*
+Write a program that asks the user for an integer and checks to determine whether or not the function is prime.
+*/
+
+import java.util.Scanner;
+
+public class Prime_num {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        
+    }
+}
