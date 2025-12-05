e595324b6653a789e61cbab8b899b947a80730a4
AidMag <27amagnus@derryfield.org>
2025-12-05 10:55:11 -0500

    Creation of all of the files
    Part C is completed.

---
+++ Unit_5/PartC.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PartC {
+    public static double vol(double r) {
+
+        r = r * r * r * Math.PI * (4.0 / 3);
+
+        return r;
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double r, vol;
+
+     
+        System.out.printf("%nEnter the radius of a circle: ");
+        r = in.nextInt();
+
+        vol = vol(r);
+
+        System.out.print(vol);
+
+        in.close();
+    }
+}
