147faf3466561a5ebe8a18e4c5d740115285e6cd
AidMag <27amagnus@derryfield.org>
2025-12-13 08:09:54 -0500

    Graphics and HW

---
+++ Unit_5/Lesson12/PartB.java
@@ -0,0 +1,14 @@
+Write a method called smallest (and a program to test it) that takes three arguments (as doubles) and returns the smallest of the three:
+double smallest(double x, double y, double z)
+
+
+public class PartB {
+
+    
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
