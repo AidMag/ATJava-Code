c187db9f5d3a110a25b9b82c514cdbafce99491d
AidMag <27amagnus@derryfield.org>
2025-12-13 08:27:40 -0500

    New
    Finished program with name

--- Unit_5/Lesson12/PartB.java
+++ Unit_5/Lesson12/PartB.java
@@ -1,14 +1,46 @@
-Write a method called smallest (and a program to test it) that takes three arguments (as doubles) and returns the smallest of the three:
-double smallest(double x, double y, double z)
+//Aidan
 
+import java.util.Scanner;
 
 public class PartB {
 
-    
+    public static double smallest(double x, double y, double z) {
+        
+        double small;
+        
+        if (x <= y && x <= z) {
+            small = x;
+        } else if (y <= x && y <= z) {
+            small = y;
+        } else {
+            small = z;
+        }
+
+        return small;
+    }
 
 
 
     public static void main(String[] args) {
+
+        double num_1=1, num_2=1, num_3=1;
         
+        Scanner in = new Scanner(System.in);
+        
+        for (int i = 1; i < 4; i++) {
+            System.out.print("Enter a number: ");
+
+            if (i == 1) {
+                num_1 = in.nextDouble();
+            } else if (i == 2) {
+                num_2 = in.nextDouble();
+            } else {
+                num_3 = in.nextDouble();
+            }
+        }
+        
+        in.close();
+
+        System.out.print("The smallest number is " + smallest(num_1, num_2, num_3));
     }
 }
