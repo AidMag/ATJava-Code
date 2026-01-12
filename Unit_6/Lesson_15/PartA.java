ccd9d8f487867c44cecd4f32d0d87aa77feb56d3
AidMag <27amagnus@derryfield.org>
2026-01-12 14:20:54 -0500

    Update PartA.java
    Done

--- Unit_6/Lesson_15/PartA.java
+++ Unit_6/Lesson_15/PartA.java
@@ -1,31 +1,39 @@
 /*
-Write a program that uses a partially filled array to scan in integer values from the user.
-The array should store a maximum of 10 values.
-The user should type in 'q' to quit entering data. 
-After data has been entered, print the elements on a single line that starts and ends with an asterisk ('*') and with each element separated by an asterisk.
-Sort the data, and then print the elements in reverse numerical order, separating values with a comma (',').
-
-For example, if the user entered the values 4, 3, 5, 2, and 0 as their integer values, your program should produce the following output,
-
-*4*3*5*2*0*
-5,4,3,2,0
-
-Important Note: When using a partially filled array, you don't want to sort the entire array.  
-You only want to sort the valid values of the partially filled array.  
-You can use an overloaded version of Arrays.sort() that lets you sort a subset of the array.
-
-For example, suppose I have an array called values that holds 10 integer values, but my current size is only equal to 5.  
-Instead of sorting the entire array, I would only want to sort the first 5 elements of the array:
-
-	Arrays.sort(values, 0, currentSize);
-
-The preceding code sorts the array called values from the element at index equals 0 up to but not including the element at index equals currentSize.
+Aidan
 */
 
-
-
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class PartA {
+    public static void main(String[] args) {
+        int counter = 0;
 
+        Scanner in = new Scanner(System.in);
+        int[] values = new int[10];
+
+        for(int i = 0;i<values.length;i++){
+            System.out.print("Enter an integer: ");
+        if (in.hasNext("q") || in.hasNext("Q")) {
+            in.next();
+            break;
+        }
+        values[i] = in.nextInt();
+        counter++;
+        }
+
+        for(int j = 0;j < counter;j++){
+            System.out.print("*");
+            System.out.print(values[j]);
+        }
+        System.out.println("*");
+
+        Arrays.sort(values, 0, counter);
+
+        for (int k = 0; k < counter; k++) {
+            System.out.print(values[counter - 1 - k]);
+            System.out.print(",");
+        }
+        in.close();
+    }
 }
