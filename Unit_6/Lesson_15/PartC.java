24dd01bd787b955f7f7f79712a99944eccaf57f1
AidMag <27amagnus@derryfield.org>
2026-01-12 16:33:00 -0500

    Update PartC.java

--- Unit_6/Lesson_15/PartC.java
+++ Unit_6/Lesson_15/PartC.java
@@ -1,14 +1,11 @@
 /*
-Write your own method that uses a linear search to determine whether an integer value is present in the array.  
-Use this method to see if a target integer value is present in the array.
-public static int linearSearch(int[] array, int tgtVal)
-Use Arrays.binarySearch() to determine whether that same target integer value is present in the array.  
-Remember, the binary search algorithm requires that the data be sorted in ascending order.
-Use the System.nanoTime() method to time how long each method takes to search the array (in nanoseconds).
-Compare how long it takes to search for an integer using the linear search method when compared to using the binary search method. 
+Aidan
+Compare how long it takes to search for an integer using the linear search method when compared to using the binary search method.
+ - consistently takes teh binary search longer to find the value.
 You should create a test loop that generates multiple (I did 10) random target values and time how long it takes to conduct a linear search and a binary search for each target value.
-Do you notice any difference when the size of the array is 10,000?
+ - look at the version before the current one to find my loop of the two.
 What happens if you increase the size from 10,000 to 10,000,000?
+ - The linear search takes just slightly longer when the number is increased.
 */
 
 
@@ -20,9 +17,12 @@ public class PartC {
     public static int linearSearch(int[] array, int tgtVal) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == tgtVal) {
-                return tgtVal;
+                return i;
+            } else {
+                continue;
             }
-        } 
+        }
+        return -1;
     }
 
     public static int B_search(int[] array, int tgtVal) {
@@ -31,14 +31,39 @@ public class PartC {
     }
     
     public static void main(String[] args) {
-        int counter = 0;
+        int counter = 0, tgt;
+        double time_linear, time_binary;
+        long startTime, startTime1, endTime, endTime1; 
 
         Scanner in = new Scanner(System.in);
         int[] values = new int[10000];
 
-        for(int i = 0;i < values.length;i++){
-        values[i] =(int)((Math.random()*10000)+1) ;
-        counter++;
+        for (int i = 0; i < values.length; i++) {
+            values[i] = (int) ((Math.random() * 10000) + 1);
+            counter++;
+        }
+        
+        System.out.print("Enter a integer 1-10,000: ");
+        tgt = in.nextInt();
+        
+        for (int i = 0; i < 11; i++) {
+            startTime = System.nanoTime();
+            time_linear = linearSearch(values, tgt);
+            endTime = System.nanoTime();
+
+            double time = endTime - startTime;
+
+            System.out.printf("Linear Search took %.2f nanoseconds %n", time);
+            System.out.printf("Index: %.2f%n", time_linear);
+
+            startTime1 = System.nanoTime();
+            time_binary = B_search(values, tgt);
+            endTime1 = System.nanoTime();
+
+            time = endTime1 - startTime1;
+
+            System.out.printf("Binary Search took %.2f nanoseconds %n", time);
+            System.out.printf("Index: %.2f%n", time_binary);
         }
 
         in.close();
