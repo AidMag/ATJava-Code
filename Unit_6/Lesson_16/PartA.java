431d15edd19934f6c67c4141239d357a507ea678
AidMag <27amagnus@derryfield.org>
2026-01-14 09:53:58 -0500

    Creating and setting up files for HW

---
+++ Unit_6/Lesson_16/PartA.java
@@ -0,0 +1,36 @@
+/*
+Write a program that uses a partially filled array of String objects to store a collection of words/phrases:
+Scan in words/phrases from the user until the user enters 'q' to quit
+Print the word/phrases in a neat manner for the user to review
+Write a method called cleanup() that takes an array of String objects as its argument,
+For each element of the array, trim any leading or trailing spaces.
+Make all letters lowercase, except for the first letter in the word/phrase.
+Capitalize the first letter of the word/phrase.
+Use the method to alter the array and print the modified words/phrases 
+A sample program run should look something like the following,
+
+Enter a word or phrase or 'q' to quit: a
+Enter a word or phrase or 'q' to quit:    bee  and   flower
+Enter a word or phrase or 'q' to quit:    dog   
+Enter a word or phrase or 'q' to quit: cats and dogs   
+Enter a word or phrase or 'q' to quit: q
+Words/Phrases (current size = 4):
+  a
+     bee  and   flower
+     dog   
+  cats and dogs   
+Modified array...
+Words/Phrases (current size = 4):
+  A
+  Bee  and   flower
+  Dog
+  Cats and dogs
+*/
+
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class PartA {
+
+}
---
+++ Unit_6/Lesson_16/PartB.java
@@ -0,0 +1,24 @@
+/*
+Write a program that has a method that takes a variable number of double values as its argument.  
+The method then calculates and returns the average of these parameters as a double value.
+
+public static double average(double ... values)
+
+Test your method for the following cases,
+average(10, 8, 9)
+average(scores)  where scores is an array of doubles equal to {100, 90, 80, 100, 80}
+
+Important Note: 
+If you use a partially filled array to store your values, then when you calculate the average using your method, 
+    you should pass a copy of your original array that only contains the valid test scores.  
+You can use the Arrays.copyOf() static method to make a new copy of the original array that is only equal in 
+    length to the current size of your partially filled array.
+*/
+
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class PartB {
+    
+}
