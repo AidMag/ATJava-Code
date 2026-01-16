eec65cf9106a954539e49d3ba49804bf5511c579
AidMag <27amagnus@derryfield.org>
2026-01-16 10:32:24 -0500

    Create PartB.java
    Starting

---
+++ Unit_6/Lesson_17/PartB.java
@@ -0,0 +1,38 @@
+
+/*
+Write a Java program that in the main program declares and initializes the following two arrays,
+
+8 6 7
+5 3 0
+
+
+
+0 7 6 5
+2 2 0 1
+8 4 5 7
+
+
+Add a public static method to your Java program that takes a 2D array as an argument and prints the elements in the array in row by column format.
+
+public static void print(int[][] values)
+
+Use the method in your program to print the two arrays that you declared and initialized.
+
+The output of your program run for Part B should look something like the following,
+
+First array:
+  8,  6,  7 
+  5,  3,  0
+
+Second array:
+  0,  7,  6,  5
+  2,  2,  0,  1
+  8,  4,  5,  7
+
+Remember, you should use a method to traverse and print the 2D arrays.  
+If you do not use a method to do the printing, you will lose points for the grading of this assignment.
+*/
+
+public class PartB {
+    
+}
