73d8c8f245470af0989f8f78b9be0214a549c700
AidMag <27amagnus@derryfield.org>
2026-01-16 10:32:12 -0500

    Create PartA.java
    Done

---
+++ Unit_6/Lesson_17/PartA.java
@@ -0,0 +1,18 @@
+/*
+Declare and initialize an array of integers that has 4 rows and 3 columns.  
+All of the elements in the array should be initialized to zero.
+
+ - int[][] values = int[4][3]; 
+
+Declare and initialize an array of integers that has 2 rows and 5 columns.  
+All of the elements in the array should be initialized to zero.
+
+ - int[][] values = int[2][5];
+
+Declare and initialize an array of integers that represents the following matrix,
+        
+ - int[][] values = {
+        {8, 6, 7}, 
+        {5, 3, 0}
+    }
+*/
