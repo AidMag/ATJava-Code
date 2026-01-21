2cafffe7ac4e6dd43c7598c38ff1802ef64abde1
AidMag <27amagnus@derryfield.org>
2026-01-21 12:25:02 -0500

    Update PartC.java
    Working on it

--- Unit_6/Lesson_18/PartC.java
+++ Unit_6/Lesson_18/PartC.java
@@ -1,11 +1,41 @@
 /*
-Write a Java program that uses an ArrayList of Integer values to keep track of temperatures:
-Scan in temperatures from user until they enter a 'q' to quit
-Determine what the lowest temperature is for the set of data
 Print each temperature on a separate line and mark each instance of the lowest temperature with "<= lowest"
 */
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PartC {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> values = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+
+        int counter = 0;
+        int lowest;
+        
+        while (true) {
+            System.out.print("Enter a temperature or enter 'q' to quit: ");
+            if (in.hasNext("q") || in.hasNext("Q")) {
+                break;
+            } else {
+                values.add(in.nextInt());
+                counter++;
+            }
+        }
+        lowest = values.get(0);
+        for (int i = 1; i < counter; i++) {
+            if (lowest > values.get(i)) {
+                lowest = values.get(i);
+            }
+        }
+
+        
+        for (int j = 0; j < counter; j++) { //There is a problem here!!
+            System.out.printf("%n%.2f", values.get(j));
+            if (values.get(j) == lowest) {
+                System.out.print("<= Lowest");
+            }
+        }
+        in.close();
+    }
 }
