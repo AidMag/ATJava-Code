4530877aff7b5828e5ff6ae1250cf780524d8d0a
AidMag <27amagnus@derryfield.org>
2026-01-21 11:45:38 -0500

    Update PartB.java
    Done

--- Unit_6/Lesson_18/PartB.java
+++ Unit_6/Lesson_18/PartB.java
@@ -1,15 +1,37 @@
 /*
-Write a Java program that prompts the users for test scores:
-Use an ArrayList to store the values as Double objects
-If the user enters 'q' then quit inputting values
-Loop through the elements in the ArrayList and print each element on a separate line with 2 decimal places of accuracy
-Calculate and display the average of the scores
+Aidan
 */
 
-
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PartB {
     public static void main(String[] args) {
+       ArrayList<Double> values = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+
+        int counter = 0;
+        double sum = 0, avg;
         
+        while (true) {
+            System.out.print("Enter a test or enter 'q' to quit: ");
+            if (in.hasNext("q") || in.hasNext("Q")) {
+                break;
+            } else {
+                values.add(in.nextDouble());
+                counter++;
+            }
+        } 
+
+        for (double value : values) {
+            sum += value;
+        }
+        avg = sum / counter;
+        
+        for (int i = 0; i < counter; i++) {
+            System.out.printf("%n%.2f", values.get(i));
+        }
+        System.out.printf("%nAverage: %.2f", avg);
+        in.close();
     }
 }
