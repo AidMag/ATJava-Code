39015939e6af2745622c8de551797da9d664c531
AidMag <27amagnus@derryfield.org>
2026-01-30 15:46:25 -0500

    Update PartB.java

--- Unit_7/HW_2/PartB.java
+++ Unit_7/HW_2/PartB.java
@@ -4,17 +4,29 @@ import java.io.FileNotFoundException;
 
 public class PartB {
    public static void main(String[] args) throws FileNotFoundException {
-       File inputFile = new File("textfiles/poem.txt");
+       File inputFile = new File("text/characters.txt");
        Scanner in = new Scanner(inputFile);
-       //in.useDelimiter("[^A-Za-z0-9]+");
-       in.useDelimiter("[, \s\t\n]+"); // for HW 
-       int count = 0;
-       while (in.hasNext()) {
-           String word = in.next();
-           System.out.println(word);
-           count++;
-       }
-       System.out.printf("%d words were scanned in.%n", count);
+       in.useDelimiter(" ");  
+       int count = 0, letter = 0, digits = 0;
+
+       while (in.hasNextLine()) {
+            String line = in.nextLine();
+            System.out.println(line);
+
+            for (int i = 0; i < line.length(); i++) {
+                char ch = line.charAt(i);
+                count++;
+
+                if (Character.isLetter(ch)) {
+                    letter++;
+                } else if (Character.isDigit(ch)) {
+                    digits++;
+                }
+            }
+        }
+    
+       
+       System.out.printf("count: %d%nletters: %d%ndigits: %d%n", count, letter, digits);
        in.close();
    }
 }
