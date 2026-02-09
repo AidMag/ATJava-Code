d164b147d23aa7eb6fde11daf1c632590317a7b3
AidMag <27amagnus@derryfield.org>
2026-02-09 16:23:51 -0500

    Update PartB.java
    Done

--- Unit_7/HW_4/PartB.java
+++ Unit_7/HW_4/PartB.java
@@ -3,24 +3,32 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /*
-The first field of each line is the name of the person
-The second field is their birth year or "n/a" if the birth year is not available
-Use Integer.parseInt() method to convert the birth year field from the file to an integer value
-Use try-catch statement to handle the NumberFormatException
-Print the name and their birth year if you were able to extract the birth year from the file
-Print a message that you don't know the birth year for that person if you were unable to extract the birth year from the file
 Your output should look like the following
 Sal was born in 2006.
 Darcy was born in 2000.
 Kim was born in 2005.
 I do not know what year Liz was born.
 Bob was born in 1999.
-
 */
 
 public class PartB {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("birthyears.txt");
         Scanner in = new Scanner(file);
+        in.useDelimiter("[, \\s]+");
+        String word, unknown;
+        int age;
+
+        while (in.hasNext()) {
+            word = in.next();
+            try {
+                age = in.nextInt();
+                System.out.printf("%s was born in %d%n", word, age);
+            } catch (Exception e) {
+                in.next();
+                unknown = "I don't know what year";
+                System.out.printf("%s %s was born.%n", unknown, word);
+            }
+       }
     }
 }
