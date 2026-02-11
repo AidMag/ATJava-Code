966aadd7259d3479a184abcdcddfd70482f9a1cd
AidMag <27amagnus@derryfield.org>
2026-02-11 09:42:00 -0500

    Starting HW

---
+++ Unit_7/HW_5/PartA.java
@@ -0,0 +1,17 @@
+/*
+Write a public static method that asks the user to enter quiz scores ranging from 0 to 100
+Scan in the quiz score as an integer value.  
+Pass an input Scanner as an argument to your method
+If the user does not provide a value in the allowed range, throw an NumberFormatException.
+If the function is unable to parse the user's input value, also throw a NumberFormatException
+Write a sample program that demonstrates how your method works and that uses a try-catch statement to check whether or not a valid quiz score was obtained.
+
+public static int getQuizScore(Scanner in) {
+}
+*/
+
+public class PartA {
+    public static void main(String[] args) {
+        
+    }
+}
