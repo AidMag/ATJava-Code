e04fc277b856078b05c5aad3db47077aaf75a19b
AidMag <27amagnus@derryfield.org>
2026-02-11 10:53:02 -0500

    Done
    Done

--- Unit_7/HW_5/PartA.java
+++ Unit_7/HW_5/PartA.java
@@ -1,17 +1,34 @@
-/*
-Write a public static method that asks the user to enter quiz scores ranging from 0 to 100
-Scan in the quiz score as an integer value.  
-Pass an input Scanner as an argument to your method
-If the user does not provide a value in the allowed range, throw an NumberFormatException.
-If the function is unable to parse the user's input value, also throw a NumberFormatException
-Write a sample program that demonstrates how your method works and that uses a try-catch statement to check whether or not a valid quiz score was obtained.
-
-public static int getQuizScore(Scanner in) {
-}
+/*  
+Aidan
 */
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class PartA {
+    public static int getQuizScore(Scanner in) {
+        int score = 0;
+        System.out.print("Enter a quiz score (0-100): ");
+        try {
+            score = in.nextInt();
+            if (score < 0 || score > 100) {
+                throw new NumberFormatException("Score must be within 0-100");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("ERROR: " + e.getMessage());
+            return 0;
+        } 
+        return score;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        try {
+            int score = getQuizScore(in);
+            System.out.println("score = " + score);
+        } catch (Exception e) {
+            System.err.println("an error occurred!");
+            System.out.println(e.getMessage());
+        }
     }
 }
