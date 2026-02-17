a629dd292c79c2f3b48acf20aba92798e6e97d15
AidMag <27amagnus@derryfield.org>
2026-02-17 18:18:48 -0500

    Update CashRegister.java
    DONE

--- Unit_8/HW_2/CashRegister.java
+++ Unit_8/HW_2/CashRegister.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class CashRegister {
     private ArrayList<Double> items = new ArrayList<>();
     private double sum;
-    private int count;
 
    public void addItem(double price) {
        items.add(price);
@@ -22,43 +21,47 @@ public class CashRegister {
    }
 
    public void delete() {
-       items.remove(count);
+       items.remove(items.size() - 1);
    }
 
    public int getCount() {
-       count = items.size();
-       return count;
+       return items.size();
    }
 
    public void clear() {
-       for (int i = 0; i < items.size(); i++) {
-           items.set(i, 0.0);
-       }
+       items.clear();
+       sum = 0;
    }
 
    public static void main(String[] args) {
 
        Scanner in = new Scanner(System.in);
        CashRegister cr = new CashRegister();
-       System.out.print("Enter cost of item: ");
+       
        while (true) {
-        System.out.print("Enter cost of item: ");
+           System.out.print("Enter cost of item: ");
            
-        
-            if (in.hasNextInt()) {
-                cr.addItem(in.nextInt());  
-            } else if (in.hasNext("t")) {
-                cr.print();
-            } else if (in.hasNext("d")) {
-                cr.delete();
-            } else if (in.hasNext("c")) {
-                cr.clear();
-            } else if (in.hasNext("q")) {
-                return;
-            }
-           
-        in.close();
+
+           if (in.hasNextDouble()) {
+               Double a = in.nextDouble();
+               cr.addItem(a);
+               continue;
+           } else if (in.hasNext()) {
+               String word = in.next();
+               if (word.equals("t")) {
+                   cr.print();
+                   System.out.println(" ");
+               } else if (word.equals("d")) {
+                   cr.delete();
+               } else if (word.equals("c")) {
+                   cr.clear();
+               } else if (word.equals("q")) {
+                   break;
+               }
+           }
+
        }
+       in.close();
    }
 }
 
