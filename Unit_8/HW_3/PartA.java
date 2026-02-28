aaf545888ce233d255e4ea9e739ad6ff60ebb761
AidMag <27amagnus@derryfield.org>
2026-02-28 12:59:42 -0500

    Update PartA.java

--- Unit_8/HW_3/PartA.java
+++ Unit_8/HW_3/PartA.java
@@ -1,11 +1,16 @@
 /*
 List an example of each of the following and write a simple program that demonstrates how these items are used:
-A Java-provided constant static value that is often used in mathematical and/or scientific calculations.
-A Java-provided static method that is often used in mathematical and/or scientific calculations.
-
+A Java-provided constant static value that is often used in mathematical and/or scientific calculations. - Math.PI
+A Java-provided static method that is often used in mathematical and/or scientific calculations. - Math.sqrt
 */
 
 
 public class PartA {
+    public static void main(String[] args) {
+        int i = 10;
+        double b = 132.2;
 
+        double sum = Math.sqrt(i + b) * Math.PI;
+        System.out.print(sum);
+    }
 }
