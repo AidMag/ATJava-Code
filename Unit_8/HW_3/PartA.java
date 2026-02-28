ee4a935577cdee6b68baca56c315d5a37761aa49
AidMag <27amagnus@derryfield.org>
2026-02-28 11:56:08 -0500

    Break

---
+++ Unit_8/HW_3/PartA.java
@@ -0,0 +1,11 @@
+/*
+List an example of each of the following and write a simple program that demonstrates how these items are used:
+A Java-provided constant static value that is often used in mathematical and/or scientific calculations.
+A Java-provided static method that is often used in mathematical and/or scientific calculations.
+
+*/
+
+
+public class PartA {
+
+}
---
+++ Unit_8/Project/Die.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Die {
+    private int value;
+
+    public Die() {
+        value = 1;
+    }
+
+    public Die(int num) {
+        value = num;
+    }
+
+    public void roll() {
+        value = (int) ((Math.random() * 6) + 1);
+    }
+
+    public int look() {
+        return value;
+    }
+
+    public void printASCII(int num) {
+        int i = value;
+        if (i == 1) {
+            System.out.printf("+--------+%n|        |%n|    0    |%n|        |%n+--------+%n");
+        } else if (i == 2) {
+            System.out.printf("+--------+%n| 0      |%n|         |%n|      0 |%n+--------+%n");
+        } else if (i == 3) {
+            System.out.printf("+--------+%n| 0      |%n|    0    |%n|      0 |%n+--------+%n");
+        } else if (i == 4) {
+            System.out.printf("+--------+%n| 0    0 |%n|         |%n| 0    0 |%n+--------+%n");
+        } else if (i == 5) {
+            System.out.printf("+--------+%n| 0    0 |%n|    0    |%n| 0    0 |%n+--------+%n");
+        } else if (i == 6) {
+            System.out.printf("+--------+%n| 0    0 |%n| 0     0 |%n| 0    0 |%n+--------+%n");
+        }
+    }
+
+    public void pair() {
+        int first = (int) ((Math.random() * 6) + 1);
+        int second = (int) ((Math.random() * 6) + 1);
+        printASCII(first);
+        printASCII(second);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Die a = new Die();
+        System.out.print("Enter 's' to set the die or 'n' to skip: ");
+        if (in.hasNext("s")) {
+            System.out.print("Enter an interger between 1-6: ");
+            int b = in.nextInt();
+            a = new Die(b);
+        } else if (!in.hasNext("s")) {
+            a = new Die(); 
+        }
+        while (true) {
+            System.out.print("Enter a command: ");
+            if (in.hasNext("q") || in.hasNext("Q")) {
+                in.close();
+                break;
+            } else if (in.hasNext("r")) {
+                in.next();
+                a.roll();
+            } else if (in.hasNext("d")) {
+                in.next();
+                a.printASCII(a.look());
+            } else if (in.hasNext("n")) {
+                in.next();
+                System.out.println(a.look());
+            } else if (in.hasNext("p")) {
+                in.next();
+                a.pair();
+            }
+        }
+    }
+}
