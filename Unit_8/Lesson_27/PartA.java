3325da5d36108733d642d66befa647fac24615cc
AidMag <27amagnus@derryfield.org>
2026-03-06 12:59:13 -0500

    StartHW

---
+++ Unit_8/Lesson_27/PartA.java
@@ -0,0 +1,15 @@
+/*
+Write a program that uses the LocalDate and LocalTime classes to display the current date and time.  
+Your output should look like the following,
+
+Today's date is 2024-01-01
+The time is 14:36:22.623383
+
+Refer to the Java API documentation to determine what package contains these two classes and to see how these classes are typically used. 
+As a comment in your program, mention the name of the package that contains these classes. 
+Also mention the names of any parent packages.
+*/
+
+public class PartA {
+    
+}
---
+++ Unit_8/Lesson_27/org/derryfield/math/Geometry.java
@@ -0,0 +1,14 @@
+package org.derryfield.math;
+
+/*
+Within the Geometry class, declare two public static methods that calculate the perimeter of a square and the surface area of a square, given the length of a side of the square:
+public static double get_perimeter_square(double s)
+public static double get_area_square(double s)
+Create a separate demo application that prompts the user for the length of the side of a square. 
+Use the package to calculate the perimeter and surface area of the specified square.
+*/
+
+
+public class Geometry {
+    
+}
