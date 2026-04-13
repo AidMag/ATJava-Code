ce485867427ce1500da7d1a0caa2beaa9e6aa601
AidMag <27amagnus@derryfield.org>
2026-04-13 12:16:30 -0400

    Update

--- App/animate/Board.java
+++ App/animate/Board.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 
 public class Board extends JPanel{
     private final int B_WIDTH = 350;
@@ -23,10 +26,25 @@ public class Board extends JPanel{
         // cast our Graphics object to a Graphics2D object.
         Graphics2D g2d = (Graphics2D) g;
 
+        // translate in the x and y directions.
+       int x_t = B_WIDTH / 2;
+       int y_t = B_HEIGHT / 2;
+       x_t = x_t - SIDE_LEN / 2;
+       y_t = y_t - SIDE_LEN / 2;
+       AffineTransform affineTransform = new AffineTransform();
+       affineTransform.translate(x_t, y_t);
+       
+       // get the transformed shape.
+       Rectangle rect = new Rectangle(0, 0, SIDE_LEN, SIDE_LEN);
+       Shape transformedShape = affineTransform.createTransformedShape(rect);
+       affineTransform.rotate(122.5, 175, 175);
+
         // set color.
         g2d.setColor(Color.MAGENTA);
 
         // draw a shape.
-        g2d.fillRect(0, 0, SIDE_LEN, SIDE_LEN);
+        g2d.fill(transformedShape);
+
+        g2d.setColor(Color.BLUE);
     }
 }
