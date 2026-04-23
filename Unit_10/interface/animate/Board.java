eaba654b86691a515c29eeff3e5f555e35c09ce6
AidMag <27amagnus@derryfield.org>
2026-04-23 12:02:15 -0400

    Update

--- Unit_10/interface/animate/Board.java
+++ Unit_10/interface/animate/Board.java
@@ -5,120 +5,98 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
+import java.awt.event.KeyEvent;
 import sound.SoundClip;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 
+public class Board extends JPanel implements KeyListener, MouseListener {
+  private final int B_WIDTH = 350;
+  private final int B_HEIGHT = 350;
+  public final int SQR_AREA = 20;
+  private int x;
+  private int y;
+  private Graphics2D g2d;
+  SoundClip blop = new SoundClip("media/blop.wav");
+  SoundClip blip = new SoundClip("media/blip.wav");
 
-public class Board extends JPanel implements KeyListener, {
-    private final int B_WIDTH = 720;
-    private final int B_HEIGHT = 720;
-    private int x = 0;
-    private int y = 0;
-    private final int DIAMETER = 20;
-    private Timer timer;
-    private final int INITIAL_DELAY = 100;
-    private final int PERIOD_INTERVAL = 25;
-    private int xSpeed = 1, ySpeed = 1;
-    private BufferedImage img;
-    private double rotateSpeed = 0.25;
-    private double rotate = 0;
-    private SoundClip blop;
+  public Board() {
+    setBackground(Color.CYAN);
+    setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+    x = (B_WIDTH / 2) - (SQR_AREA / 2);
+    y = (B_HEIGHT / 2) - (SQR_AREA / 2);
+    this.setFocusable(true);
+    this.addKeyListener(this);
+    this.addMouseListener(this);
+  }
 
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
 
-    public Board() {
-       // set background color of the board and default size.
-       setBackground(Color.CYAN);
-       setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+    g2d = (Graphics2D) g;
+    Graphics2D g2d = this.g2d;
+    g2d.setColor(Color.MAGENTA);
+    g2d.fillOval(x, y, this.SQR_AREA, this.SQR_AREA);
+  }
 
+  public void center() {
+    x = (B_WIDTH / 2) - (SQR_AREA / 2);
+    y = (B_HEIGHT / 2) - (SQR_AREA / 2);
 
-       blop = new SoundClip("media/blop.wav");
-       blop.open();
+    repaint();
+  }
 
-       this.setFocusable(true);
-       this.addKeyListener(this);
-    }   
+  public void mouseCircle(int x, int y) {
+    g2d.setColor(Color.CYAN);
+    g2d.fillOval(x, y, SQR_AREA, SQR_AREA);
+  }
 
+  public void blopSound() {
+    blop.open();
+    blop.play();
+  }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        AffineTransform affineTransform = new AffineTransform();
-        affineTransform.translate(x - img.getWidth() / 2.0, y - img.getHeight() / 2.0);
-        affineTransform.rotate(rotate, img.getWidth() / 2.0, img.getHeight() / 2.0);
+  public void blipSound() {
+    blip.open();
+    blip.play();
+  }
 
-        if (img != null) {
-            g2d.drawImage(img, affineTransform, null);
-        }
+  public void keyPressed(KeyEvent e) {
+  }
+
+  public void keyTyped(KeyEvent e) {
+    char keyType = e.getKeyChar();
+    if (keyType == ' ') {
+      blopSound();
+      center();
     }
+  }
 
-    public int getB_WIDTH() {
-        return B_WIDTH;
-    }
+  public void keyReleased(KeyEvent e) {
+  }
 
-    public int getB_HEIGHT() {
-        return B_HEIGHT;
-    }
+  public void mousePressed(MouseEvent e) {
+  }
 
-    public int getX() {
-        return x;
-    }
+  public void mouseEntered(MouseEvent e) {
 
-    public int getY() {
-        return y;
-    }
+  }
 
-    public int getDIAMETER() {
-        return DIAMETER;
-    }
+  public void mouseReleased(MouseEvent e) {
 
-    public Timer getTimer() {
-        return timer;
-    }
+  }
 
-    public int getINITIAL_DELAY() {
-        return INITIAL_DELAY;
-    }
+  public void mouseClicked(MouseEvent e) {
+    int mouseX = e.getX();
+    int mouseY = e.getY();
+    x = mouseX;
+    y = mouseY;
+    repaint();
+    blipSound();
+  }
 
-    public int getPERIOD_INTERVAL() {
-        return PERIOD_INTERVAL;
-    }
+  public void mouseExited(MouseEvent e) {
 
-    public int getxSpeed() {
-        return xSpeed;
-    }
-
-    public int getySpeed() {
-        return ySpeed;
-    }
-
-    public BufferedImage getImg() {
-        return img;
-    }
-
-    public double getRotateSpeed() {
-        return rotateSpeed;
-    }
-
-    public double getRotate() {
-        return rotate;
-    }
-
-
-    public void keyTyped(KeyEvent e) {
-        
-    }
-
-
-    public void keyPressed(KeyEvent e) {
-        blop.play();
-    }
-
-
-    public void keyReleased(KeyEvent e) {
-
-    }
+  }
 }
