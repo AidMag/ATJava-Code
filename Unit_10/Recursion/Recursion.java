349df5c145e4ddc7fe8d145ba9abc3a51f3066cc
AidMag <27amagnus@derryfield.org>
2026-05-01 10:58:11 -0400

    Update Recursion.java

--- Unit_10/Recursion/Recursion.java
+++ Unit_10/Recursion/Recursion.java
@@ -4,16 +4,46 @@ public class Recursion {
         if (n == 1 || n == 0) {
             return 1;
         } else if (n < 0) {
-            throw new IllegalArgumentException("n must be a nonnegitive integer.");
+            throw new IllegalArgumentException("n must be a non-negitive integer.");
         } else {
             return n * factorial(n - 1);
         }
     }
 
+    public static void printInverted(int n) {
+        if (n == 1 || n == 0) {
+            System.out.print("* ");
+        } else if (n < 0) {
+            throw new IllegalArgumentException("n must be a non-negitive integer.");
+        } else {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            printInverted(n - 1);
+        }
+        System.out.println();
+    }
+
+    public static void printTriangle(int n) {
+        if (n == 1 || n == 0) {
+            System.out.print("* ");
+        } else if (n < 0) {
+            throw new IllegalArgumentException("n must be a non-negitive integer.");
+        } else {
+            printTriangle(n - 1);
+            System.out.println();
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+
+        }
+    }
+
+   
     public static void main(String[] args) {
-       System.out.printf("3! = %d%n", factorial(3));
-       System.out.printf("4! = %d%n", factorial(4));
-       System.out.printf("5! = %d%n", factorial(5));
-   }
+        printInverted(5);
+        printTriangle(5);
+    }
 
 }
