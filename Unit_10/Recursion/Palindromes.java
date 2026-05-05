b6dcc0f02ef14331c5b6b3ebce8e72aa3c53f7a3
AidMag <27amagnus@derryfield.org>
2026-05-05 10:52:52 -0400

    Create Palindromes.java

---
+++ Unit_10/Recursion/Palindromes.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Palindromes {
+    public static boolean isPalr(String a) {
+
+    }
+
+    public static boolean isPali(String a) {
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+
+        String a;
+
+        System.out.print("Enter a word: ");
+        a = in.next();
+
+        
+
+
+
+        in.close();
+    }
+}
