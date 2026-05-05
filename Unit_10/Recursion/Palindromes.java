0d6d18cb00725598be7ea199842687c855eb8786
AidMag <27amagnus@derryfield.org>
2026-05-05 16:37:02 -0400

    Update Palindromes.java

--- Unit_10/Recursion/Palindromes.java
+++ Unit_10/Recursion/Palindromes.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
 public class Palindromes {
-    public static boolean isPalr(String a) {
-
+    public static void isPalr(String[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = a.length - 1; j < a.length; j++) {
+                
+            }
+        }
     }
 
-    public static boolean isPali(String a) {
-
+    public static void isPali(String[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = a.length - 1; j < a.length; j++) {
+                
+            }
+        }
     }
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
-
-        String a;
+        Palindromes check = new Palindromes();
 
         System.out.print("Enter a word: ");
-        a = in.next();
-
+        String[] word = in.next().split("");
         
-
-
+        check.isPalr(word);
+        check.isPali(word);
 
         in.close();
     }
