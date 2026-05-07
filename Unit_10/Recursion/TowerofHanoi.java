f0c5e64cfb1e7f69772062455520435708e44951
AidMag <27amagnus@derryfield.org>
2026-05-07 08:57:02 -0400

    Update TowerofHanoi.java
    Creating the general structure for the class.

--- Unit_10/Recursion/TowerofHanoi.java
+++ Unit_10/Recursion/TowerofHanoi.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 
 public class TowerofHanoi {
+
+    public static void hanoi(int n, int start, int end) {
+
+    }
+    
+    public static int getInterim(int start, int end) {
+
+    }
+    
+    public static void printMove(int start, int end) {
+
+    }
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         in.close();
-        
+
     }
 }
