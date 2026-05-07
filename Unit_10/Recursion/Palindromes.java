32e3ad07fa0600eb4f007fdcc502717f02a1efa4
AidMag <27amagnus@derryfield.org>
2026-05-07 08:36:39 -0400

    Update Palindromes.java

--- Unit_10/Recursion/Palindromes.java
+++ Unit_10/Recursion/Palindromes.java
@@ -1,31 +1,51 @@
 import java.util.Scanner;
 
 public class Palindromes {
-    public static void isPalr(String[] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = a.length - 1; j < a.length; j++) {
-                
-            }
-        }
-    }
 
-    public static void isPali(String[] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = a.length - 1; j < a.length; j++) {
-                
+    public static boolean isPali(String s) {
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        int i = 0, j = s.length() - 1;
+        for (int a = s.length() / 2; a > 0; a--) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
             }
         }
+        return true;
     }
     
+    public static boolean isPalr(String s) {
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        if (s.length() == 0 || s.length() == 1) {
+            return true;
+        }
+        if (s.charAt(0) == s.charAt(s.length() - 1)) {
+            return isPalr(s.substring(1, s.length() - 1));
+        } else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Palindromes check = new Palindromes();
 
         System.out.print("Enter a word: ");
-        String[] word = in.next().split("");
+        String word = in.nextLine();
         
-        check.isPalr(word);
-        check.isPali(word);
+        if (isPalr(word)){
+            System.out.println(word + " is a palindrome (recersive)");
+        } else {
+            System.out.println(word + " is not a palindrome (recersive)");
+        }
+
+        if (isPali(word)){
+            System.out.println(word + " is a palindrome (iteritive)");
+        } else {
+            System.out.println(word + " is not a palindrome (iteritive)");
+        }
+
 
         in.close();
     }
